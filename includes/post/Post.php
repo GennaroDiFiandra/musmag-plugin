642ed271d0d2798f478e5c02c6531b6ab892f287
Gennaro Di Fiandra <gennaro@futurefashionsolution.com>
2023-10-26 10:23:21 +0200

    feat: improve class variables name

--- includes/post/Post.php
+++ includes/post/Post.php
@@ -9,7 +9,7 @@ class Post
   private string $name;
   private string $plural_name;
   private bool $is_public;
-  private bool $has_gutenberg;
+  private bool $in_rest_api;
   private int $menu_position;
   private string $menu_icon;
   private array $features;
@@ -21,23 +21,23 @@ class Post
     return $this->name;
   }
 
-  public function set($name, $plural_name, $is_public, $has_gutenberg, $menu_position, $menu_icon, $features=['title'])
+  public function set($name, $plural_name, $is_public, $in_rest_api, $menu_position, $menu_icon, $features=['title'])
   {
     $this->name = $name;
     $this->plural_name = $plural_name;
     $this->is_public = $is_public;
-    $this->has_gutenberg = $has_gutenberg;
+    $this->in_rest_api = $in_rest_api;
     $this->menu_position = $menu_position;
     $this->menu_icon = $menu_icon;
     $this->features = $features;
     $this->labels = [
       'name' => _x(ucwords($this->plural_name), 'Post type plural name', 'musmag-plugin'),
-      'name' => _x(ucwords($this->name), 'Post type singular name', 'musmag-plugin'),
+      'singular_name' => _x(ucwords($this->name), 'Post type singular name', 'musmag-plugin'),
     ];
     $this->args = [
       'labels' => $this->labels,
       'public' => $this->is_public,
-      'show_in_rest' => $this->has_gutenberg,
+      'show_in_rest' => $this->in_rest_api,
       'menu_position' => $this->menu_position,
       'menu_icon' => $this->menu_icon,
       'supports' => $this->features,
